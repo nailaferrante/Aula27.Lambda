c4b9df93653105f1b5ecc44805f2f89bd06560f1
FIC <FIC@info.local>
2017-11-24 20:15:37 -0200

    Criação do repositório

---
+++ Atividade.cs
@@ -0,0 +1,12 @@
+namespace Lambda
+{
+    public abstract class Atividade
+    {
+        public static double subtrair(int v1, int v2){
+            return v1-v2;
+        }
+        public static string multiplicar(int v1, int v2){
+            return "O resultado é "+(v1*v2);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        // {
+        //     Func<int,int,int> rs = soma;// o rs se apropriou das propriedades do método soma
+        //     Console.WriteLine(rs(10,5));
+        // }
+        // {
+        //     Func<int,int,double> res = Atividade.subtrair;
+        //     Console.WriteLine(res(10,5));
+        // }
+        {
+            Func<int,int,string> ser = Atividade.multiplicar;
+            Console.WriteLine(ser(10,5));
+        }
+        static int soma(int v1, int v2){
+            return v1+v2;
+            }
+    }
+}
